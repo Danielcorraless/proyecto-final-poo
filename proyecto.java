65640503729afcf98923702f6684ead2f833639f
Andres Barraza <andres_bava.fcfm@uas.edu.mx>
2021-02-01 17:16:08 -0700

    Comentarios sobre como trabajarlo

--- proyecto.java
+++ proyecto.java
@@ -2,5 +2,8 @@ public class proyecto{
     public static void main(String[] args) {
         System.out.println("Hola topacio");
         //Comentario de prueba
+        //Cada cosa que se agregue al codigo intenten comentarla, poner no se su firma pa saber quien se la rifo y quien rompio el codigo
+        //Tambien es importante que cuando hagan cambios pongan un comentario en el commit, solo algo global para saber que se agrego y que se quito
+        //Ahorita estoy viendo monster int, mas al rato intento hacer un task list para saber como empezar el codigo, seria bueno que nos repartieramos funciones o tareas de este, luego vemos. -andres
     }
 }
